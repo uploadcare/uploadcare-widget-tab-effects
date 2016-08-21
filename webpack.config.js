a44673b85dc96da91d27e12b0a46fa9ac9beedce
Nikolay Zherdev <nickzherdev@gmail.com>
2016-08-21 23:01:44 +0300

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  devtool: 'sourcemap',
+  entry: [
+    './src/uploadcare.tab-effects.js'
+  ],
+  module: {
+    loaders: [
+       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'babel' },
+       { test: /\.html$/, loader: 'raw' },
+       { test: /\.scss$/, loader: 'style!css!postcss-loader!sass' },
+       { test: /\.css$/, loader: 'style!css!postcss-loader' },
+       { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/font-woff" },
+       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
+    ]
+  },
+  plugins: [
+    // Injects bundles in your index.html instead of wiring all manually.
+    // It also adds hash to all injected assets so we don't have problems
+    // with cache purging during deployment.
+  ],
+
+  postcss: function () {
+      return [autoprefixer, precss];
+  }
+};
