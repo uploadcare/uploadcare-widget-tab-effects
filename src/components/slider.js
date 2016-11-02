aca6bc3a6de20ddcd4c12f45a3afde3432d4e4d5
Nikolay Zherdev <Nikolay.Zherdev@wkfs-frc.local>
2016-11-02 17:39:18 +0300

    Slider as <Input type="range">

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,7 +1,6 @@
 'use strict'
 import ejs from '../../node_modules/ejs/ejs';
 import sliderTemplate from '../templates/slider.html';
-import sliderStyles from '../styles/slider.scss';
 import IdGenerator from '../tools/IdGenerator.js';
 
 const $ = uploadcare.jQuery;
@@ -11,82 +10,36 @@ export default class Slider {
     this.container = container;
     this.onChangeHandler = null;
     this.maxValue = maxValue;
-    this.POINTER_ID = "pointer_" + IdGenerator.Generate();
+    this.RANGE_ID = "range_" + IdGenerator.Generate();
 
   }
 
   render(parentEl = this.container, value = 0) {
     this.container = parentEl;
     let markupStr = ejs.render(sliderTemplate, {
-      pointerId: this.POINTER_ID,
-      styles: sliderStyles
+      pointerId: this.RANGE_ID,
+      min: 0,
+      max: this.maxValue,
+      value: value
     });
     parentEl.html(markupStr);
     this.setupHandlers(parentEl);
-
-    this.currentPos = Math.round(100 / this.maxValue * value);
-
-    this.$pointer.css("left", "calc(" + this.currentPos + "% - 8px)");
   }
 
   setupHandlers(parentEl) {
-    this.$pointer = 
-    $(parentEl).find("#" + this.POINTER_ID);
+    this.$range = 
+    $(parentEl).find("." + this.RANGE_ID);
 
-    this.$pointer.on("mousedown touchstart", ev => { 
-      return this.pointerMouseDown(ev);
+    this.$range.on("change", ev => { 
+      return this.change(ev);
     });   
   }
 
-  pointerMouseDown(ev) {
-
-    this.multiplyer = 100 / ( this.$pointer.parent().width() );
-    this.leftOffset = this.$pointer.parent().offset().left;
-    ev.preventDefault();
-    ev.stopPropagation();
-    ev.bubbles = false;
-
-    $("body").mousemove(ev => {
-      return this.bodyMouseMove(ev); 
-    });
-
-    $("body").on("touchmove", ev => {
-      return this.bodyTouchMove(ev); 
-    });
-
-    $("body").on("mouseup touchend", ev => { 
-      return this.bodyMouseUp(ev)
-    });
-  }
-
-  bodyMouseMove(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-    ev.bubbles = false;
-    return this.updatePoinerPos(ev.pageX);
-  }
-
-  bodyTouchMove(ev) {
-    console.log(ev.originalEvent.touches[0]);
-    return this.updatePoinerPos(ev.originalEvent.touches[0].clientX);
-  }
-
-  updatePoinerPos(pageX) {
-    let pointerPos = ((pageX - this.leftOffset) * this.multiplyer);
-    pointerPos = Math.max(0, pointerPos);
-    pointerPos = Math.min(100, pointerPos);
-    pointerPos = Math.round(pointerPos);
-
-    this.$pointer.css("left", "calc(" + pointerPos + "% - 8px)");
-    
-    if(this.onChangeHandler && this.currentPos != pointerPos) {
-      this.onChangeHandler(Math.round(this.maxValue / 100 * pointerPos));
+  change(ev) {
+    console.log(ev);
+    if(this.onChangeHandler) {
+      this.onChangeHandler(ev.currentTarget.value);
     }
-    this.currentPos = pointerPos;
-  }
-
-  bodyMouseUp(ev) {
-    $("body").off();
   }
 
   onChange( handler ) {
