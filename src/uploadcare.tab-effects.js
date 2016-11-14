9ace69c2b00fec436e9a3b5dbf618fe975fe2dc7
Nikolay Zherdev <Nikolay.Zherdev@wkfs-frc.local>
2016-11-14 22:41:15 +0300

    show preview just after image upload

--- src/uploadcare.tab-effects.js
+++ src/uploadcare.tab-effects.js
@@ -3,30 +3,36 @@ import EffectsModel from './models/effectsModel';
 
 function effectsTab(container, button, dialogApi, settings) {
 
-  let filePromises = dialogApi.fileColl.get();
 // getting first image for preview;
-  let isFileTaken = false; 
+  let isFileTaken = false;
+
+  const fileResolver = function (fileInfo) {
+    if (isFileTaken) {
+      return;
+    }
+
+    if (fileInfo.isImage) {
+      isFileTaken = true;
+    }
+    
+    let model = new EffectsModel('ucarecdn.com/', fileInfo.originalImageInfo.width, fileInfo.originalImageInfo.height);
+    model.parseUrl(fileInfo.cdnUrl);
+    let previewView = new PreviewView(container, model);
+    previewView
+      .render()
+      .then(type => {
+        fileInfo.cdnUrl = model.getPreviewUrl();
+        dialogApi.resolve();
+      });
+  }
+
+  dialogApi.fileColl.onAdd.add((promise, i) => {
+    promise.then(fileResolver);
+  });
+  let filePromises = dialogApi.fileColl.get();
 
   filePromises.forEach((promise, i) => {
-    promise.then((fileInfo) => {
-      
-      if(isFileTaken) {
-        return; 
-      }
-
-      if(fileInfo.isImage) {
-        isFileTaken = true;
-      }
-        let model = new EffectsModel('ucarecdn.com/', fileInfo.originalImageInfo.width, fileInfo.originalImageInfo.height);
-        model.parseUrl(fileInfo.cdnUrl);
-        let previewView = new PreviewView(container, model);
-        previewView
-          .render()
-          .then(type => {
-            fileInfo.cdnUrl = model.getPreviewUrl();
-            dialogApi.resolve();
-          });
-    });
+    promise.then(fileResolver);
   });
 }
 
