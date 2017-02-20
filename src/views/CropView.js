f0b3b51ee450e89fbffb66ccb64fe26b05c03129
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-02-20 03:18:48 +0300

    Add crop initialization to CropView

--- src/views/CropView.js
+++ src/views/CropView.js
@@ -1,4 +1,5 @@
 import BaseView from '../tools/BaseView/BaseView'
+import CropButton from '../components/CropButton/CropButton'
 import classnames from './CropView.pcss'
 import template from './CropView.html'
 
@@ -8,19 +9,51 @@ class CropView extends BaseView {
 
     this.cn = Object.assign({}, this.cn, classnames)
     this.template = template
+    this.cropWidget = null
+    this.currentCrop = (props.store.getState()).settings.crop[0]
   }
 
   templateDidMount() {
     const {cn} = this
-    const {store, container, onDone, onCancel} = this.props
+    const {uc, store, container, onDone, onCancel} = this.props
+    const state = store.getState()
     const done = container.querySelector(`.${cn.done}`)
     const cancel = container.querySelector(`.${cn.cancel}`)
+    const cropButtons = container.querySelector(`.${cn['crop-buttons']}`)
+
+    state.settings.crop.forEach(crop => {
+      const cropButton = new CropButton({
+        uc,
+        crop,
+        onClick: () => this.cropWidget.setCrop(crop),
+      })
+
+      cropButtons.appendChild(cropButton.render())
+    })
 
     done.addEventListener('click', () => onDone())
     cancel.addEventListener('click', () => {
       store.setEffect('crop', {})
       onCancel()
     })
+  }
+
+  imageDidLoad() {
+    super.imageDidLoad()
+
+    if (!this.cropWidget) {
+      const {image, currentCrop} = this
+      const {store, uc} = this.props
+      const state = store.getState()
+      const {rotate} = state.appliedEffects
+      const {width, height} = state.image.originalImageInfo
+
+      const size = rotate && !!~[90, 270].indexOf(rotate)
+        ? [height, width]
+        : [width, height]
+
+      this.cropWidget = new uc.crop.CropWidget(uc.jQuery(image), size, currentCrop)
+    }
   }
 }
 
