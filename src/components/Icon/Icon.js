46bc5e8e69c23f5d8a7f1bd2cefd3f8e9d2f7ba7
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-03-31 18:45:46 +0300

    Change getIcon func to Icon component

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,47 @@
+import createNode from '../../tools/create-node'
+import cn from './Icon.pcss'
+import blur from '../../images/blur.svg'
+import crop from '../../images/crop.svg'
+import enhance from '../../images/enhance.svg'
+import flip from '../../images/flip.svg'
+import grayscale from '../../images/grayscale.svg'
+import mirror from '../../images/mirror.svg'
+import rotate from '../../images/rotate.svg'
+import sharp from '../../images/sharp.svg'
+
+const icons = {
+  blur,
+  crop,
+  enhance,
+  flip,
+  grayscale,
+  mirror,
+  rotate,
+  sharp,
+}
+
+const Icon = (name) => {
+  if (!icons[name]) {
+    throw new Error(`Icon with name "${name}" doesn't exist.`)
+  }
+
+  let element
+
+  const getElement = () => {
+    if (!element) {
+      render()
+    }
+
+    return element
+  }
+
+  const render = () => {
+    element = createNode(icons[name])
+
+    element.classList.add(cn['icon'])
+  }
+
+  return {getElement}
+}
+
+export default Icon
