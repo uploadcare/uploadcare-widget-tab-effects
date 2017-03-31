91ac48903d43019bd44dd19938606957150a79ed
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-03-31 18:54:03 +0300

    Update Crop component. Lazy render.

--- src/components/CropButton/CropButton.js
+++ src/components/CropButton/CropButton.js
@@ -1,43 +1,51 @@
+import createNode from '../../tools/create-node'
 import cn from './CropButton.pcss'
 import template from './CropButton.html'
 
 const CropButton = (props) => {
-  const {uc, crop, onClick} = props
-  const prefered = crop.preferedSize
-  let title
+  let element
 
-  if (prefered) {
-    const gcd = uc.utils.gcd(prefered[0], prefered[1])
+  const getElement = () => {
+    if (!element) {
+      render()
+    }
 
-    title = `${prefered[0] / gcd}:${prefered[1] / gcd}`
-  }
-  else {
-    title = uc.locale.t('dialog.tabs.preview.crop.free')
+    return element
   }
 
-  const elementContainer = document.createElement('div')
+  const render = () => {
+    const {uc, crop, onClick} = props
+    const prefered = crop.preferedSize
+    let title
 
-  elementContainer.innerHTML = template({
-    title,
-    cn,
-  })
+    if (prefered) {
+      const gcd = uc.utils.gcd(prefered[0], prefered[1])
 
-  const element = elementContainer.querySelector(`.${cn['crop-button']}`)
-  const icon = element.querySelector(`.${cn['crop-button__icon']}`)
+      title = `${prefered[0] / gcd}:${prefered[1] / gcd}`
+    }
+    else {
+      title = uc.locale.t('dialog.tabs.preview.crop.free')
+    }
 
-  if (prefered) {
-    const size = uc.utils.fitSize(prefered, [30, 30], true)
+    element = createNode(template({
+      title,
+      cn,
+    }))
 
-    icon.style.width = `${Math.max(20, size[0])}px`
-    icon.style.height = `${Math.max(12, size[1])}px`
+    const icon = element.querySelector(`.${cn['crop-button__icon']}`)
+
+    if (prefered) {
+      const size = uc.utils.fitSize(prefered, [30, 30], true)
+
+      icon.style.width = `${Math.max(20, size[0])}px`
+      icon.style.height = `${Math.max(12, size[1])}px`
+    }
+    else {
+      icon.classList.add('uploadcare--crop-sizes__icon_free')
+    }
+
+    element.addEventListener('click', () => onClick(element))
   }
-  else {
-    icon.classList.add('uploadcare--crop-sizes__icon_free')
-  }
-
-  element.addEventListener('click', () => onClick(element))
-
-  const getElement = () => element
 
   return {getElement}
 }
