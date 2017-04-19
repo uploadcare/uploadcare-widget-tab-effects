a81ca45f7ffe8c07b16e8083a32268fd86620574
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-04-19 18:15:16 +0300

    Fix paths in images index

--- src/images/index.js
+++ src/images/index.js
@@ -1,11 +1,11 @@
-import blur from '../blur.svg'
-import crop from '../crop.svg'
-import enhance from '../enhance.svg'
-import flip from '../flip.svg'
-import grayscale from '../grayscale.svg'
-import mirror from '../mirror.svg'
-import rotate from '../rotate.svg'
-import sharp from '../sharp.svg'
+import blur from './blur.svg'
+import crop from './crop.svg'
+import enhance from './enhance.svg'
+import flip from './flip.svg'
+import grayscale from './grayscale.svg'
+import mirror from './mirror.svg'
+import rotate from './rotate.svg'
+import sharp from './sharp.svg'
 
 export {
   blur,
