b49b4ee28a4b3153c27c8acc44f9c3c7452c0b27
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-08-21 20:37:00 +0300

    Add config file for start npm script

---
+++ scripts/start.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  port: 3127,
+  server: './demo',
+  serveStatic: ['./dist'],
+  files: ['./demo', './dist'],
+  rewriteRules: [{
+    match: 'http://ucarecdn.com/libs/widget-tab-effects/1.x/uploadcare.tab-effects.min.js',
+    replace: 'uploadcare.tab-effects.js',
+  }],
+}
