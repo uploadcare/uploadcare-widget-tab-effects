903a7246384be80529302eeceaf2371fecad9029
comontes <1701089+comontes@users.noreply.github.com>
2022-11-08 10:38:36 -0300

    Add new locale: Spanish (es). (#42)
    * Created es locale
    
    * Updated index.js locale

---
+++ src/locale/es.js
@@ -0,0 +1,16 @@
+export default {
+  effects: {
+    captions: {
+      blur: 'Desenfocar',
+      crop: 'Recortar',
+      enhance: 'Mejorar',
+      flip: 'Voltear',
+      grayscale: 'Escala de grises',
+      invert: 'Invertir',
+      mirror: 'Reflejar',
+      rotate: 'Rotar',
+      sharp: 'Nitidez',
+    },
+    apply: 'Aplicar',
+  },
+}
