4e644d751b557a34f3364e423c921c72d61be99b
joelgrondrup <joel@joel.biz>
2023-06-01 11:36:29 +0200

    Swedish locale
    Adding swedish locale

---
+++ src/locale/sv.js
@@ -0,0 +1,17 @@
+export default {
+    effects: {
+      captions: {
+        blur: 'Suddig',
+        crop: 'Beskära',
+        enhance: 'Öka',
+        flip: 'Flip',
+        grayscale: 'Grayscale',
+        invert: 'Invertera',
+        mirror: 'Återspegla',
+        rotate: 'Rotera',
+        sharp: 'Skärpa',
+      },
+      apply: 'Använda',
+    },
+  }
+  
