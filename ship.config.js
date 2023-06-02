18f7e2cb79efd9a5cd9b3e270018d084f1b230dd
nd0ut <nd0ut.me@gmail.com>
2023-06-02 14:30:18 +0300

    chore: disable internal shipjs changelog generator

--- ship.config.js
+++ ship.config.js
@@ -3,4 +3,6 @@ module.exports = {
   pullRequestReviewers: ['nd0ut'],
   afterPublish: ({exec}) => exec('node ./scripts/publish-to-s3.js'),
   beforeCommitChanges: ({exec}) => exec('npm run changelog'),
+  // Here we update changelog using legacy kacl tool
+  updateChangelog: false,
 }
