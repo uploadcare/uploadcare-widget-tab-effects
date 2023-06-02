d6153b6351524c7c03356ff5a33ea0cc6e1351b2
nd0ut <nd0ut.me@gmail.com>
2023-06-02 14:32:17 +0300

    chore: upload assets to CDN before publishing npm package to make able to re-run CI workflow if any issues arised

--- ship.config.js
+++ ship.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mergeStrategy: {toSameBranch: ['master']},
   pullRequestReviewers: ['nd0ut'],
-  afterPublish: ({exec}) => exec('node ./scripts/publish-to-s3.js'),
+  beforePublish: ({exec}) => exec('node ./scripts/publish-to-s3.js'),
   beforeCommitChanges: ({exec}) => exec('npm run changelog'),
   // Here we update changelog using legacy kacl tool
   updateChangelog: false,
