1a33bd89f4e4ad86feeb76deaf2dfaeb38ec23a8
nd0ut <nd0ut.me@gmail.com>
2023-06-02 15:29:03 +0300

    chore: remove kacl and legacy changelog generation

--- ship.config.js
+++ ship.config.js
@@ -2,5 +2,4 @@ module.exports = {
   mergeStrategy: {toSameBranch: ['master']},
   pullRequestReviewers: ['nd0ut'],
   beforePublish: ({exec}) => exec('node ./scripts/publish-to-s3.js'),
-  beforeCommitChanges: ({exec}) => exec('npm run changelog'),
 }
