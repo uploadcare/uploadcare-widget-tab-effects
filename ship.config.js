9dce8257320a965b1dab993e34291bac58091a48
Alex Gusev <alx.gsv@gmail.com>
2026-07-13 17:35:44 +0600

    ci: migrate npm publish to staged publishing (#54)

--- ship.config.js
+++ ship.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   mergeStrategy: {toSameBranch: ['master']},
   pullRequestReviewers: ['nd0ut'],
+  publishCommand: ({tag}) => `npm stage publish --tag ${tag}`,
   beforePublish: ({exec}) => exec('node ./scripts/publish-to-s3.js'),
 }
